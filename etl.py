8d0e4d21ab3c97eb888b309fbfb27770aa35e717
曹杰 <CJ@CJmacbookpro.local>
2017-06-13 18:13:24 +0800

    modify etl process and model;

--- etl.py
+++ etl.py
@@ -1,11 +1,6 @@
 # -*- coding: UTF-8 -*-
 
 import pandas as pd
-import numpy as np
-from scipy.stats import skew
-import seaborn as sns
-import matplotlib
-import matplotlib.pyplot as plt
 
 LOCAL_CONFIG = {
 
@@ -14,7 +9,9 @@ LOCAL_CONFIG = {
 
 class ETL:
     def __init__(self):
-        pass
+        df_train, df_test = self.get_data()
+        self.train = df_train
+        self.test = df_test
 
     @staticmethod
     def get_data():
@@ -22,44 +19,62 @@ class ETL:
         df_test = pd.read_csv('data/test.csv')
         return df_train, df_test
 
+    @staticmethod
+    def replace_by_dummies(df_X, col):
+        dummy1 = pd.get_dummies(df_X.loc[:, col], prefix=col)
+        df_X.drop([col], axis=1, inplace=True)
+        df_X = pd.concat([df_X, dummy1], axis=1)
+        return df_X
+
     def overview_missing(self):
-        df_train, df_test = self.get_data()
-        total = df_train.isnull().sum().sort_values(ascending=False)
-        percent = (df_train.isnull().sum() / df_train.isnull().count()).sort_values(ascending=False)
+        total = self.train.isnull().sum().sort_values(ascending=False)
+        percent = (self.train.isnull().sum() / self.train.isnull().count()).sort_values(ascending=False)
         missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
         return missing_data
 
     def etl_data(self):
-        df_train, df_test = self.get_data()
-        all_data = pd.concat((df_train.loc[:, 'MSSubClass':'SaleCondition'],
-                              df_test.loc[:, 'MSSubClass':'SaleCondition']))
+        # 删除 missing value 比例大于15%的 column
+        types = self.train.dtypes.to_frame(name='type')
+        miss_percent = self.overview_missing()
+        cols = pd.merge(miss_percent, types, left_index=True, right_index=True)
+        delete_cols = cols.loc[(cols['Percent'] >= 0.15) & (cols['type'] != 'object')].index
+        delete_cols = list(delete_cols)
+        # print(delete_cols)
+        all_data = pd.concat([self.train.loc[:, 'MSSubClass':'SaleCondition'],
+                              self.test.loc[:, 'MSSubClass':'SaleCondition']], axis=0, ignore_index=True)
+        all_data.drop(delete_cols, axis=1, inplace=True)
 
-        # log transform the target:
-        df_train["SalePrice"] = np.log1p(df_train["SalePrice"])
+        # one hot encode category features
+        numeric_feats = self.train.dtypes[self.train.dtypes != "object"].index
+        categ_feats = self.train.dtypes[self.train.dtypes == "object"].index
+        for col in categ_feats:
+            all_data = self.replace_by_dummies(all_data, col)
 
-        # log transform skewed numeric features:
-        numeric_feats = all_data.dtypes[all_data.dtypes != "object"].index
-
-        skewed_feats = df_train[numeric_feats].apply(lambda x: skew(x.dropna()))  # compute skewness
-        skewed_feats = skewed_feats[skewed_feats > 0.75]
-        skewed_feats = skewed_feats.index
-
-        all_data[skewed_feats] = np.log1p(all_data[skewed_feats])
-        all_data = pd.get_dummies(all_data)
-        # filling NA's with the mean of the column:
+        # fill missing data
         all_data = all_data.fillna(all_data.mean())
-        print(all_data)
 
         # creating matrices for sklearn:
-        X_train = all_data[:df_train.shape[0]]
-        X_test = all_data[df_train.shape[0]:]
-        y = df_train.SalePrice
+        X_train = all_data[:self.train.shape[0]]
+        X_test = all_data[self.train.shape[0]:]
+        y = self.train.loc[:, 'SalePrice']
+
+        return X_train, X_test, y
+
+    def get_result(self):
+        X_train, X_test, y = self.etl_data()
+
+        has_nan = X_train.isnull().any().any()
+        print('if has nan: ', has_nan)
+
+        X_train = X_train.values
+        X_test = X_test.values
+        y = y.values
+
+        return X_train, X_test, y
+
 
 if __name__ == '__main__':
     etl = ETL()
-    etl.get_data()
-
-
-    # res = etl.overview_missing()
-    # print(res)
-
+    X_train, X_test, y = etl.get_result()
+    print(X_train)
+    print(y)
--- models.py
+++ models.py
@@ -1,3 +1,105 @@
+# -*- coding: UTF-8 -*-
+
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+from sklearn import metrics
+from etl import ETL
+from sklearn.model_selection import ShuffleSplit
+import math
+
+LOCAL_CONFIG = {
+    'METHODS': ['lr', 'lasso', 'dt', 'rf', 'gb', 'adb'],
+    'METHOD': 'gb'
+}
+
+
+class Model:
+    def __init__(self):
+        self.etl = ETL()
+
+    def get_train_data(self):
+        X_train, X_test, y = self.etl.get_result()
+        y = np.array(list(map(lambda x: math.log(x + 1, math.e), y)))
+        return X_train, X_test, y
+
+    @staticmethod
+    def normalize(X_train, X_test):
+        """
+        scaler and nomalizer
+        """
+        # scaler = preprocessing.StandardScaler().fit(X)
+        # X = scaler.transform(X)
+        # X_noEx = scaler.transform(X_noEx)
+        normalizer = preprocessing.Normalizer().fit(X_train)
+        X_train = normalizer.transform(X_train)
+        X_test = normalizer.transform(X_test)
+        return X_train, X_test
+
+    @staticmethod
+    def rmsle(y_pred, y):
+        # map1 = list(map(lambda x: math.log(x + 1, math.e), y_pred))
+        # map2 = list(map(lambda x: math.log(x + 1, math.e), y))
+        res = np.sqrt(metrics.mean_squared_error(y_pred, y))
+        return res
+
+    def method_pattern(self, model):
+        X_train1, X_test1, y = self.get_train_data()
+        X_train1, X_test1 = self.normalize(X_train1, X_test1)
+
+        # training
+        kf = ShuffleSplit(n_splits=5, test_size=0.25, random_state=0)
+        rmsles = []
+        for train_indices, test_indices in kf.split(X_train1):
+            X_train = X_train1[train_indices, :]
+            y_train = y[train_indices]
+            X_test = X_train1[test_indices, :]
+            y_test = y[test_indices]
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_test)
+
+            rmsle = self.rmsle(y_pred, y_test)
+            rmsles.append(rmsle)
+            print('rmsle: ', rmsle)
+        print('mean of rmsle: ', np.mean(rmsles))
+        return np.mean(rmsles)
+
+    def try_diff_method(self, method):
+        if method == 'lr':
+            from sklearn.linear_model import LinearRegression
+            lr = LinearRegression()
+            self.method_pattern(lr)
+        elif method == 'lasso':
+            from sklearn.linear_model import Lasso
+            lasso = Lasso(alpha=0.1)
+            self.method_pattern(lasso)
+        elif method == 'dt':
+            from sklearn.tree import DecisionTreeRegressor
+            dt = DecisionTreeRegressor()
+            self.method_pattern(dt)
+        elif method == 'rf':
+            from sklearn.ensemble import RandomForestRegressor
+            rf = RandomForestRegressor()
+            self.method_pattern(rf)
+        elif method == 'gb':
+            from sklearn.ensemble import GradientBoostingRegressor
+            gb = GradientBoostingRegressor()
+            self.method_pattern(gb)
+        elif method == 'adb':
+            from sklearn.ensemble import AdaBoostRegressor
+            adab = AdaBoostRegressor()
+            self.method_pattern(adab)
+        else:
+            print('do nothing')
+
+    def get_result(self):
+        self.try_diff_method(LOCAL_CONFIG['METHOD'])
+
+if __name__ == '__main__':
+    model = Model()
+    model.get_result()
+
+
 
 
 
